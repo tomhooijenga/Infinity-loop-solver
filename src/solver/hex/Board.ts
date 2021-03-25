2d2272ee1dd472f4daebd40282ee1ee106aa7e8c
thooijenga <thooijenga@snelstart.nl>
2021-03-25 18:40:01 +0100

    Fix hex neighbours on even columns

--- src/solver/hex/Board.ts
+++ src/solver/hex/Board.ts
@@ -5,20 +5,31 @@ export class Board extends BaseBoard {
     public neighbours(tile: Tile): Tile[] {
         const {x, y} = tile;
 
-        // start: 1,1
-        // 0,0
-        // 1,0
-        // 2,1
-        // 1,2
-        // 0,2
-        // 0,1
-        return [
-            [-1, -1],
-            [0, 1],
-            [1, 0],
-            [0, 1],
-            [-1, 1],
-            [-1, 0]
-        ].map(([dx, dy]) => this.getOrNone(x + dx, y + dy));
+        // odd column down
+        let deltas: [number, number][];
+
+        if (x % 2) {
+            // start: 1,1
+            deltas = [
+                [0, -1],    // 1,0
+                [1, 0],     // 2,1
+                [1, 1],     // 2,2
+                [0, 1],     // 1,2
+                [-1, 1],    // 0,2
+                [-1, 0],    // 0,1
+            ]
+        } else {
+            // start 2,1
+            deltas = [
+                [0, -1],    // 2,0
+                [1, -1],    // 3,0
+                [1, 0],     // 3,1
+                [0, 1],     // 2,2
+                [-1, 0],    // 1,1
+                [-1, -1]    // 1,0
+            ]
+        }
+
+        return deltas.map(([dx, dy]) => this.getOrNone(x + dx, y + dy));
     }
 }
