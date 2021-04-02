e298a5dc293a2316cdb9bb5e12e7087dae022789
thooijenga <thooijenga@snelstart.nl>
2021-04-02 12:30:22 +0200

    Fix setting of num_sides

--- site/src/use-board.ts
+++ site/src/use-board.ts
@@ -1,4 +1,4 @@
-import { reactive } from 'vue'
+import {reactive, watch} from 'vue'
 import { BoardData, boards, empty } from '@/boards'
 import { Tile } from '../../src/solver/base/Tile'
 import { DirectionUtil } from '../../src/solver/base/DirectionUtil'
@@ -17,17 +17,19 @@ const board = reactive<BoardData>({
 
 const clearBoard = () => {
   Object.assign(board, empty())
-  DirectionUtil.NUM_SIDES = 4
 }
 
 const loadBoard = (name: keyof typeof boards) => {
   Object.assign(board, boards[name]())
-  DirectionUtil.NUM_SIDES = board.type === 'square' ? 4 : 6
 }
 
 function setTile (index: number, tile: Tile) {
   board.tiles[index] = tile
 }
+
+watch(() => board.type, () => {
+  DirectionUtil.NUM_SIDES = board.type === 'square' ? 4 : 6
+})
 
 function generateBoard () {
   let generator
@@ -38,9 +40,7 @@ function generateBoard () {
     generator = new Generator(new HexBoard([]), Object.values(hex))
   }
 
-  const tiles = generator.generate(board.width, board.height)
-
-  board.tiles = tiles
+  board.tiles = generator.generate(board.width, board.height)
 }
 
 export function useBoard () {
