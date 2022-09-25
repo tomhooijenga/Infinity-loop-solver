0740ee75e51942a68b01dff738c95f838bd41179
Tom <tomhooijenga@gmail.com>
2022-09-25 22:49:05 +0200

    Replace calculated values with static

--- src/components/triangle/Tile.vue
+++ src/components/triangle/Tile.vue
@@ -1,19 +1,17 @@
 <template>
-  <svg :viewBox="`-50 -${height / 2} 100 ${height}`" class="w-full h-full">
+  <svg viewBox="-50 -43.30127018922193 100 86.60254037844386" class="w-full h-full">
     <template v-if="tile.type === 'None'">
-      <circle r="2" class="fill-red" :cy="cy" />
+      <circle r="2" class="fill-red" cy="14.433756729740644" />
     </template>
 
     <template v-else-if="tile.type === 'End'">
       <circle
-        :r="cy"
-        :cy="cy"
+        r="14.433756729740644"
+        cy="14.433756729740644"
         class="fill-transparent stroke-red stroke-[6px]"
       />
       <path
-        :d="`M ${Math.cos((Math.PI * 2 * 30) / 360) * cy} ${
-          Math.sin((Math.PI * 2 * 30) / 360) * cy
-        } L 25 0`"
+        d="M 12.5 7.216878364870321 L 25 0"
         class="fill-transparent stroke-red stroke-[6px]"
       />
     </template>
@@ -22,7 +20,7 @@
       <circle
         r="50"
         cx="50"
-        :cy="height / 2"
+        cy="43.30127018922193"
         class="fill-transparent stroke-red stroke-[6px]"
       />
     </template>
@@ -31,33 +29,33 @@
       <circle
         r="50"
         cx="0"
-        :cy="-height / 2"
+        cy="-43.30127018922193"
         class="fill-transparent stroke-red stroke-[6px]"
       />
 
       <circle
         r="50"
         cx="50"
-        :cy="height / 2"
+        cy="43.30127018922193"
         class="fill-transparent stroke-light stroke-[10px]"
       />
       <circle
         r="50"
         cx="50"
-        :cy="height / 2"
+        cy="43.30127018922193"
         class="fill-transparent stroke-red stroke-[6px]"
       />
 
       <circle
         r="50"
         cx="-50"
-        :cy="height / 2"
+        cy="43.30127018922193"
         class="fill-transparent stroke-light stroke-[10px]"
       />
       <circle
         r="50"
         cx="-50"
-        :cy="height / 2"
+        cy="43.30127018922193"
         class="fill-transparent stroke-red stroke-[6px]"
       />
     </template>
@@ -67,9 +65,6 @@
 <script setup lang="ts">
 import { PropType } from "vue";
 import { Tile } from "@/lib/base/Tile";
-
-const height = Math.sqrt(100 ** 2 - 50 ** 2);
-const cy = (height / 2 + height / 2 - height / 2) / 3;
 
 defineProps({
   tile: {
