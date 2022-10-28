e01fe9497c6c2e0539e49f68fab2fb9febb9d523
Tom <tomhooijenga@gmail.com>
2022-10-28 16:41:52 +0200

    Add start of render

--- src/canvas/resize.ts
+++ src/canvas/resize.ts
@@ -1,11 +1,10 @@
 import { BoardData } from "@/boards";
+import { Tile } from "@/lib/base/Tile";
 
 export function resize(board: BoardData, ctx: CanvasRenderingContext2D, maxW: number, maxH: number) {
   const canvas = ctx.canvas;
 
   const ratio = squareRatio(board);
-
-  console.log(ratio);
 
   if (maxH * ratio > maxW) {
     canvas.width = maxW;
@@ -28,4 +27,54 @@ function squareRatio(board: BoardData): number {
   const y = board.height
 
   return x / y;
+}
+
+export function render(board: BoardData, ctx: CanvasRenderingContext2D) {
+
+  const tileWidth = ctx.canvas.width / board.width;
+
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  board.tiles.forEach((tile) => renderSquare(ctx, tile, tileWidth))
+}
+
+function renderSquare(ctx: CanvasRenderingContext2D, tile: Tile, size: number) {
+  const { x, y, direction } = tile;
+  const dx = size * x;
+  const dy = size * y;
+
+  // ctx.lineWidth = 1;
+  // ctx.strokeStyle = 'white';
+  // ctx.strokeText(`${x},${y} ${tile.type}`, dx + 10, dy + 10)
+
+  square[tile.type]?.(ctx, tile, size, dx, dy, );
+}
+
+const square = {
+  None(ctx: CanvasRenderingContext2D, tile: Tile, size, x, y) {
+    const cx = x + size / 2
+    const cy = y + size / 2
+
+    // ctx.lineWidth = 1;
+    // ctx.strokeStyle = 'white';
+    // ctx.strokeText(`${x},${y} ${tile.type}`, cx + 10, cy + 10)
+
+    ctx.beginPath();
+    ctx.arc(cx, cy, size / 100 * 2, 0, 2 * Math.PI)
+    ctx.fillStyle = 'red';
+    ctx.fill();
+  }
+}
+
+function triangle(ctx: CanvasRenderingContext2D, w, x, y) {
+  const h = w * 0.8660254037844386;
+  const dx = w * x;
+  const dy = h * y;
+
+  ctx.beginPath();
+
+  ctx.moveTo(dx + w / 2, dy);
+  ctx.lineTo(dx + w, dy + h);
+  ctx.lineTo(dx, dy + h);
+  ctx.fill();
 }
--- src/components/triangle/Grid.vue
+++ src/components/triangle/Grid.vue
@@ -7,7 +7,7 @@
 <script setup lang="ts">
 import { computed, CSSProperties, onMounted, PropType, ref, watch, watchEffect } from "vue";
 import { Tile as TileType } from "@/lib/base/Tile";
-import { resize } from "@/canvas/resize";
+import { render, resize } from "@/canvas/resize";
 import { useBoard } from "@/use-board";
 
 const props = defineProps({
@@ -57,55 +57,22 @@ watchEffect((onCleanup) => {
   observer = new ResizeObserver((entries) => {
     const { width, height } = entries[0].contentRect;
     resize(board, ctx, width, height);
+    render(board, ctx);
   });
 
   observer.observe(wrapper.value);
 });
 
-watch(board, () => {
+watch([() => board.width, () => board.height], () => {
   if (!ctx || !wrapper.value) {
     return;
   }
 
   const { width, height } = wrapper.value.getBoundingClientRect()
   resize(board, ctx, width, height);
+});
+
+watch(() => board.tiles, () => {
+  render(board, ctx);
 })
-//
-// onMounted(() => {
-//   const c = canvas.value!;
-//   const ctx = c.getContext("2d")!;
-//
-//   const x = (props.x + 1) / 2;
-//   const y = props.y * 0.8660254037844386;
-//
-//   const { width, height } = c.getBoundingClientRect();
-//
-//   console.log(c.getBoundingClientRect());
-//
-//   c.width = height * (x / y);
-//   c.height = height;
-// });
-//
-// watchEffect(() => {
-//   const c = canvas.value;
-//   if (!c) return;
-//
-//   const ctx = c.getContext("2d")!;
-//   ctx.clearRect(0, 0, c.width, c.height);
-//   const w = c.width / props.x;
-//   props.tiles.forEach(({ x, y }) => triangle(ctx, w, x, y));
-// });
-
-function triangle(ctx: CanvasRenderingContext2D, w, x, y) {
-  const h = w * 0.8660254037844386;
-  const dx = w * x;
-  const dy = h * y;
-
-  ctx.beginPath();
-
-  ctx.moveTo(dx + w / 2, dy);
-  ctx.lineTo(dx + w, dy + h);
-  ctx.lineTo(dx, dy + h);
-  ctx.fill();
-}
 </script>
