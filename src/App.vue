d88e32dc31a96e0df13aa3296c526a34dac05797
Tom <tomhooijenga@gmail.com>
2023-10-18 23:26:22 +0200

    Remove unused styles

--- src/App.vue
+++ src/App.vue
@@ -30,24 +30,10 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
+<style>
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
-
-@layer utilities {
-  .bg-gradient-radial {
-    background-image: radial-gradient(var(--tw-gradient-stops));
-  }
-
-  .clip-triangle {
-    clip-path: polygon(50% 0%, 100% 100%, 0% 100%);
-  }
-
-  .clip-hexagon {
-    clip-path: polygon(75% 0, 100% 50%, 75% 100%, 25% 100%, 0 50%, 25% 0);
-  }
-}
 
 h1 {
   @apply text-3xl mt-6 mb-4;
